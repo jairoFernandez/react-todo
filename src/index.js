0f012ab9ef6d4157b148635a2f12fa11c3b608ee
JairoFernandez <jairofg12@gmail.com>
2018-08-03 00:47:02 -0500

    Add reducers, actions
    Basic configurationþ

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux'
+import configureStore from "./store/configureStore";
+//import todoApp from './redux/reducers';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -9,9 +14,14 @@ import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 document.title = "TODO APP";
 
+const store = configureStore();
+
 ReactDOM.render(
-    <HashRouter basename='/'>
-        <App />
-    </HashRouter>,
+
+    <Provider store={store}>
+        <HashRouter basename='/'>
+            <App />
+        </HashRouter>
+    </Provider>,
     document.getElementById('root'));
 registerServiceWorker();
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,59 @@
 import React, { Component } from 'react';
 import { UserList } from '../components/user/UserList';
+import { connect } from "react-redux";
+import * as userActions from '../actions/userAction';
 
-export class Home extends Component {
+class Home extends Component {
+
+    state = {
+        user: {
+            name: ''
+        }
+    }
+
+    onNameChange = (event) => {
+        const user = this.state.user;
+        user.name = event.target.value;
+        this.setState({ user })
+    }
+
+    onClickSave= () => {
+        this.props.dispatch(userActions.createUser(this.state.user));
+    }
+
     render() {
         return (
             <div className="animated bounceInUp" >
                 <h1>TODO APP</h1>
+                <div className="FormUsers">
+                    <h2>Add users</h2>
+                    <input
+                        type="text"
+                        onChange={this.onNameChange}
+                        value={this.state.user.name}    
+                    />
+
+                    <input 
+                        type="submit"
+                        value="Save"
+                        onClick={this.onClickSave}
+                    />
+                </div>
+                <hr/>
                 <UserList />
             </div>
         );
     }
 }
+
+function mapStateToProps(state, ownProps){
+    return {
+        users: state.users
+    }
+}
+
+function mapDispatchToProps(){
+
+}
+
+export default connect(mapStateToProps)(Home);
